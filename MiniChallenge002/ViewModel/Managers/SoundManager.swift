abd247cafdc96d28c78906f91f1a2ab6a87c6acf
Henrique Assis <henriquebatista061@gmail.com>
2022-11-11 15:24:14 -0300

    Criado alguns utils

---
+++ MiniChallenge002/ViewModel/Managers/SoundManager.swift
@@ -0,0 +1,8 @@
+//
+//  SoundManager.swift
+//  MiniChallenge002 iOS
+//
+//  Created by Henrique Batista de Assis on 11/11/22.
+//
+
+import Foundation
---
+++ MiniChallenge002/ViewModel/Utils/VectorUtils.swift
@@ -0,0 +1,8 @@
+//
+//  VectorUtils.swift
+//  MiniChallenge002 iOS
+//
+//  Created by Henrique Batista de Assis on 11/11/22.
+//
+
+import Foundation
