3e0fc024e49fb70d750e3734e623338bd58fe704
Henrique Assis <henriquebatista061@gmail.com>
2022-11-11 15:11:42 -0300

    Projeto criado

---
+++ MiniChallenge002/MiniChallenge002 iOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  MiniChallenge002 iOS
+//
+//  Created by Henrique Batista de Assis on 11/11/22.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
