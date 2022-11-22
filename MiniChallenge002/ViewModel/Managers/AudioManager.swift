f6f4ad199bb6afb21c460dcee469a5cea6f068c0
Henrique Assis <henriquebatista061@gmail.com>
2022-11-22 14:28:14 -0300

    feito uma corrotina no TimeUtils, iniciando AudioManager

---
+++ MiniChallenge002/ViewModel/Managers/AudioManager.swift
@@ -0,0 +1,14 @@
+//
+//  AudioManager.swift
+//  MiniChallenge002 iOS
+//
+//  Created by Henrique Batista de Assis on 22/11/22.
+//
+
+import Foundation
+import AVFoundation
+import SpriteKit
+
+class AudioManager {
+    
+}
