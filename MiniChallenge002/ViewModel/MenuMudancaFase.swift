f13bc016236f755befef86e8cf52b6f36c0b895a
Luana Moraes <90534155+luumoraes@users.noreply.github.com>
2022-11-30 15:08:22 -0300

    commit

--- MiniChallenge002/ViewModel/MenuMudancaFase.swift
+++ MiniChallenge002/ViewModel/MenuMudancaFase.swift
@@ -15,11 +15,12 @@ class MenuMudancaFase: SKScene {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.scaleMode = .resizeFill
         addChild(botaoJogar())
+        addChild(criarLixeira())
     }
 
     // criando o botao de jogar
     func botaoJogar() -> SKNode  {
-        let botaoJogar = ButtonPrefab(positionPoint: CGPoint(x: 200, y: -80), spriteWidth: 150, labelText: "Jogar", fontSize: 25) {
+        let botaoJogar = ButtonPrefab(positionPoint: CGPoint(x: 250, y: -100), spriteWidth: 150, labelText: "Jogar", fontSize: 25) {
             let transition:SKTransition = SKTransition.fade(withDuration: 1) //determinar a transição entre telas
             let scene:SKScene = MovimentoPersonagem(size: self.size) // chamando tela principal
             
@@ -31,7 +32,7 @@ class MenuMudancaFase: SKScene {
     
     // criando node que será a lixeira
     func criarLixeira() -> SKNode {
-        let lixeira = ButtonPrefab(color: .orange, colorSwitch: .orange, positionPoint: CGPoint(x: 20, y: -100), labelText: "Lixeira", rectangleSize: CGSize(width: 200, height: 400)) {
+        let lixeira = ButtonPrefab(color: .orange, colorSwitch: .orange, positionPoint: CGPoint(x: -200, y: -100), labelText: "Lixeira", rectangleSize: CGSize(width: 150, height: 200)) {
     
         }
         
